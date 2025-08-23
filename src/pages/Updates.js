63b22c9158e2eb61271173a462732aab12a3af87
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-08-23 15:27:11 +0430

    update footer.js

--- src/pages/Updates.js
+++ src/pages/Updates.js
@@ -20,7 +20,6 @@ export default function Updates() {
     }, 15000);
 
     return () => clearInterval(interval);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <div className="updates-panel">
